e423084cde247c4c03cfe63b8cd13bf9981a50eb
Hector Avalos <gerardo.avalos97@hotmail.com>
2023-06-28 00:00:26 -0600

    Add recaptcha public key log.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,10 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
-import {memo} from 'react';
 import {GoogleReCaptchaProvider} from 'react-google-recaptcha-v3';
 
-const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
+const MyApp = ({Component, pageProps}: AppProps) => {
+  console.log(process.env.NEXT_PUBLIC_RECAPTCHA_KEY as string);
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY as string}
@@ -18,6 +18,6 @@ const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
       <Component {...pageProps} />
     </GoogleReCaptchaProvider>
   );
-});
+};
 
 export default MyApp;
