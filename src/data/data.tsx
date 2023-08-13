18d41777449773440b9b11e0eaafc5cc3aeec00c
Hector Avalos <gerardo.avalos97@hotmail.com>
2023-08-12 21:05:42 -0600

    Update CV data.

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -16,7 +16,7 @@ import heroImage from '../images/header-background.jpg';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.png';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.png';
+import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import testimonial1 from '../images/testimonial-1.jpg';
@@ -59,6 +59,12 @@ export const SectionId = {
 
 export type SectionId = (typeof SectionId)[keyof typeof SectionId];
 
+const getAge = (): string => {
+  var ageDifMs = Date.now() - new Date("1997-04-17").getTime();
+  var ageDate = new Date(ageDifMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970).toString();
+}
+
 /**
  * Hero section
  */
@@ -68,14 +74,22 @@ export const heroData: IHero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a <strong className="text-stone-100">Full Stack Software Engineer (BE-Heavy)</strong> based on Guadalajara,
-        JAL, MX. My last job was at <strong className="text-stone-100">Gold Media Tech LLC</strong> helping elder
-        americans find the Medicare Plan that covers all/most of their needs (drugs coverage, hospitals, doctors, etc)
-        based off of their personalized selection.
+        I'm a <strong className="text-stone-100">Software Engineer</strong> based on Guadalajara,
+        Jalisco, México. Currently working at &nbsp;
+        <strong className="text-stone-100">
+          <a href="https://www.epam.com/">EPAM Systems México</a>
+        </strong>.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time I love to be learning new stuff, for instance, right now I'm learning about solar energy
-        and how to implement it to make my house off-grid.
+        In my free time time I love to learn new stuff. Also I love playing videogames, two of my
+        favorite videogames are &nbsp;
+        <strong className="text-stone-100">
+          <a href="https://en.wikipedia.org/wiki/Silent_Hill_2">Silent Hill 2</a>
+        </strong>
+        &nbsp;and&nbsp;
+        <strong className="text-stone-100">
+          <a href="https://en.wikipedia.org/wiki/Stray_(video_game)">Stray</a>
+        </strong>
       </p>
     </>
   ),
@@ -98,15 +112,15 @@ export const heroData: IHero = {
  */
 export const aboutData: IAbout = {
   profileImageSrc: profilepic,
-  description: `I'm a 26 year old Software Engineer. I started coding at the age of 14 because back then I wanted to know
-  how videogames were made. So I started reading about software development. The first programming language I learned was C.
-  My main goal is to become a Tech Lead, I've been trying to develop my soft-skills to become a good Tech Lead.`,
+  description: `I'm a ${getAge()} year old Software Engineer. I started writing code at the age of 14 (in 2011) because back then I was very curious
+  about how videogames worked. So I started reading a lot about software development. The first programming language I learned was C and one
+  of its libraries for videogames, Allegro. My main goal is to become a Tech Lead so I've been developing my soft-skills to become a good one.`,
   aboutItems: [
-    {label: 'Location', text: 'Guadalajara, MX.', Icon: MapIcon},
-    {label: 'Age', text: '26', Icon: CalendarIcon},
-    {label: 'Interests', text: 'Cars, Software Engineering, Electronics.', Icon: SparklesIcon},
+    {label: 'Location', text: 'Guadalajara, Mexico.', Icon: MapIcon},
+    {label: 'Age', text: getAge(), Icon: CalendarIcon},
+    {label: 'Interests', text: 'Engineering, Electronics, Videogames.', Icon: SparklesIcon},
     {label: 'Study', text: 'University of Guadalajara', Icon: AcademicCapIcon},
-    {label: 'Last Employment', text: 'Gold Media Tech LLC.', Icon: BuildingOffice2Icon},
+    {label: 'Employment', text: 'EPAM Systems México.', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -176,13 +190,19 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: IPortfolioItem[] = [
   {
+    title: 'EPAM Systems México S de RL de CV',
+    description: 'Full Stack Software Engineer.',
+    url: 'https://epam.com',
+    image: porfolioImage4,
+  },
+  {
     title: 'Connie Health',
     description: 'Full Stack Software Engineer.',
     url: 'https://conniehealth.com',
     image: porfolioImage2,
   },
   {
-    title: 'Gold Media Tech',
+    title: 'Gold Media Tech LLC',
     description: 'Full Stack Software Engineer.',
     url: 'https://goldmediatech.com',
     image: porfolioImage3,
@@ -192,12 +212,6 @@ export const portfolioItems: IPortfolioItem[] = [
     description: 'Software Developer.',
     url: 'https://vectralis.com',
     image: porfolioImage1,
-  },
-  {
-    title: 'Tecnianet',
-    description: 'Freelance Software Engineer.',
-    url: 'https://tecnianet.com.mx/',
-    image: porfolioImage4,
   },
 ];
 
@@ -210,13 +224,37 @@ export const education: ITimelineItem[] = [
     location: 'Centro de Enseñanza Técnica Industrial.',
     title: 'Tecnólogo en Informática y Computación',
   },
+  {
+    date: 'Feb 2020 - Aug 2020',
+    location: 'BEDU.org',
+    title: 'Full Stack JavaScript (150h. bootcamp)',
+  },
+  {
+    date: 'May 2019 - Dec 2019',
+    location: 'BEDU.org',
+    title: 'Full Stack Python (150h. bootcamp)',
+  },
 ];
 
 export const experience: ITimelineItem[] = [
   {
+    date: 'Jul 2023 - Currently',
+    location: 'Guadalajara, Jalisco. MX. (Remote)',
+    title: 'Full Stack Software Engineer @ EPAM Systems México',
+    content: (
+      <p>
+        Working in a project for an EPAM client. In this project I've been using technologies like
+        ReactJS, Storybook, Material UI, Apollo GraphQL, AWS DynamoDB, Docker and docker-compose and
+        tools like Figma, Atlassian Jira and Atlassian Confluence, all this implemented in a
+        serverless architecture. The rendering of the project is dynamic which requires us developers
+        to write "generic" components that depend on the JSON schema provided to the app.
+      </p>
+    ),
+  },
+  {
     date: 'Jul 2020 - March 2023',
-    location: 'Gold Media Tech LLC',
-    title: 'Software Engineer',
+    location: 'Boston, MA. (Remote)',
+    title: 'Software Engineer @ Gold Media Tech LLC',
     content: (
       <p>
         Working for a Medicare Advisors company. I’ve written code for both Front and Back End using technologies such
@@ -228,8 +266,8 @@ export const experience: ITimelineItem[] = [
   },
   {
     date: 'Aug 2017 - Jul 2020',
-    location: 'Zapopan, JAL.',
-    title: 'Software developer',
+    location: 'Zapopan, Jalisco. MX.',
+    title: 'Software developer @ Vectralis-Baumann Automation',
     content: (
       <p>
         R&D Software Developer. I used to develop apps that helped the Automation area in their daily tasks (PLCs,
